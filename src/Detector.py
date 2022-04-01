5da7bc2e3d693081708cdacbceef52183f3788ff
elenagiraldo3 <elenagiraldo3@gmail.com>
2022-04-01 17:26:03 +0200

    Detección de coches con filtro de color

--- src/Detector.py
+++ src/Detector.py
@@ -1,66 +1,104 @@
 from typing import List
-
 import numpy as np
-
-from .keras_retinanet import models
-from .keras_retinanet.utils.gpu import setup_gpu
-from .keras_retinanet.utils.image import preprocess_image, resize_image
-
-from .Vehicle import Vehicle
+import cv2.cv2 as cv2
+from Vehicle import Vehicle
 
 
-class Detector:
-    def __init__(self, path_to_model: str = "./model/model.h5", score: float = 0.5):
-        """
-        Detector will use RetinaNet to detect vehicles.
-        :param str path_to_model: The path where the model is stored.
-        :param float score: Score of confidance. 0.5 by default.
-        """
+# Extraer segmentos grandes para las carreteras
+def extractLargerSegment(maskROAD):
+    contours, _ = cv2.findContours(maskROAD.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        # select GPU
-        gpu = "0"
-        setup_gpu(gpu)
+    maxA = 0
+    maskTemp = np.zeros_like(maskROAD)
 
-        # Load RetinaNet model
-        self._model = models.load_model(path_to_model, backbone_name="resnet50")
+    if len(contours) > 0:
+        for h, cnt in enumerate(contours):
+            if cv2.contourArea(cnt) > maxA:
+                cntMax = cnt
+                maxA = cv2.contourArea(cnt)
+        mask = np.zeros(maskROAD.shape, np.uint8)
+        cv2.drawContours(maskTemp, [cntMax], 0, 255, -1)
+        maskROAD = cv2.bitwise_and(maskROAD, maskTemp)
+    return maskROAD
 
-        # Set score confidance
-        self._score = score
 
-    def detect_vehicles(self, img: np.ndarray, row: int, col: int) -> List:
-        """
-        Detect vehicles in sub-image.
-        :param np.ndarray img: The image where detection will be. Must be (250x250x3).
-        :param int row: Padding row.
-        :param int col: Padding col.
-        :return: List with all detected vehicles.
-        """
+# Procesamos la imagen para quitar los trozos que no son de carreteras
+def post_process(img):
+    kernel = np.ones((5, 5), np.uint8)
+    img_out = cv2.erode(img, kernel, iterations=3)
+    kernel = np.ones((20, 20), np.uint8)
+    img_out = cv2.dilate(img_out, kernel, iterations=5)
 
-        # Preprocess image
-        image = preprocess_image(img)
-        image, scale = resize_image(image)
+    img_out = extractLargerSegment(img_out)
 
-        # Predict Vehicles
-        boxes, scores, labels = self._model.predict_on_batch(
-            np.expand_dims(image, axis=0)
-        )
+    return img_out
 
-        # Scale boxes
-        boxes /= scale
 
-        # Generate Tree objects
-        vehicles = []
-        for box, score, _ in zip(boxes[0], scores[0], labels[0]):
-            # Boxes are sorted from 1->0
-            if score < 0.5:
-                break
+# Detectar con un filtro de color qué zonas de la imagen es una carretera
+def detect_road(img: np.ndarray) -> np.ndarray:
+    ilowH = 40
+    ihighH = 100
 
-            box = box.astype(int)
+    ilowS = 20
+    ihighS = 60
+    ilowV = 10
+    ihighV = 90
 
-            x1, y1, x2, y2 = box
-            # print(x1, y1, x2, y2)
-            width = x2 - x1
-            height = y2 - y1
-            vehicles.append(Vehicle(y1 + row, x1 + col, width, height))
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_hsv = np.array([ilowH, ilowS, ilowV])
+    higher_hsv = np.array([ihighH, ihighS, ihighV])
+    mask = cv2.inRange(hsv, lower_hsv, higher_hsv)
+    mask = post_process(mask)
+    img_segmented = img.copy()
+    img_segmented[mask == 0] = (0, 0, 0)
+    return img
 
-        return vehicles
+
+def detect_vehicles(img: np.ndarray, row: int, col: int) -> List:
+    """
+    Detect vehicles in sub-image with color filter.
+    param np.ndarray img: The image where detection will be.
+    param int row: Padding row.
+    param int col: Padding col.
+    :return: List with all detected vehicles.
+    """
+
+    # segmented_image = detect_road(img)
+
+    ilowH = 40
+    ihighH = 100
+
+    ilowS = 20
+    ihighS = 55
+    ilowV = 10
+    ihighV = 90
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_hsv = np.array([ilowH, ilowS, ilowV])
+    higher_hsv = np.array([ihighH, ihighS, ihighV])
+    output_mask = cv2.inRange(hsv, lower_hsv, higher_hsv)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
+    output_mask = cv2.erode(output_mask, kernel=kernel, iterations=1)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    output_mask = cv2.dilate(output_mask, kernel=kernel, iterations=1)
+
+    linesP = cv2.HoughLinesP(output_mask, 1, np.pi / 180, 50, None, 50, 10)
+
+    if linesP is not None:
+        for i in range(0, len(linesP)):
+            l = linesP[i][0]
+            cv2.line(output_mask, (l[0], l[1]), (l[2], l[3]), (0, 0, 0), 3, cv2.LINE_AA)
+
+    # Detectar los distintos contornos que aparecen en la imagen y quedarse
+    # con aquel que presente mayor área
+    contours, _ = cv2.findContours(output_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    area_max = 40
+    area_min = 15
+    vehicles = []
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+        if (area_min <= w <= area_max) and (area_min <= h <= area_max) and np.abs(h - w) < 7:
+            vehicles.append(Vehicle(y + row, x + col, w, h))
+
+    return vehicles
--- src/traffic_detector.py
+++ src/traffic_detector.py
@@ -1,23 +1,22 @@
-from typing import List, Tuple
+from typing import List
 import numpy as np
+import Detector
 
 
 class TrafficDetector:
-    def __init__(self, nn):
+    def __init__(self):
         """
         TrafficDetector constructor.
-        :param NeuralNetwork nn: the neural network which we will use.
         """
         self._img = None
         self._coordinates = None
-        self._nn = nn  # Neural Network
         self._vehicles = []
+        self._detector = Detector
 
-    def get_cars_number_from_image(self, img: np.ndarray, coordinates: Tuple) -> List:
+    def get_cars_number_from_image(self, img: np.ndarray) -> List:
         """
         Recognize vehicles in image.
-        :param np.ndarray img: The target image.
-        :param tuple coordinates: The coordinates of the image.
+        param np.ndarray img: The target image.
         :return: the list with all detected vehicles.
         """
         self._img = img
@@ -29,7 +28,7 @@ class TrafficDetector:
 
     def _slide(self):
         """
-        Iterates around the image and calls NN to detect vehicles in sub-image.
+        Iterates around the image and calls Detector to detect vehicles in sub-image.
         """
 
         STEP = 250
@@ -38,11 +37,11 @@ class TrafficDetector:
 
         for col in range(0, cols, STEP):
             for row in range(0, rows, STEP):
-                vehicles = self._nn.detect_vehicles(
+                vehicles = self._detector.detect_vehicles(
                     self._img[row: row + STEP, col: col + STEP], row, col
                 )
                 self._vehicles.append(vehicles)
 
     @staticmethod
     def get_cars_density_from_image(self):
-        return len(self._vehicles)/(self._img.shape[0] * self._img.shape[1])
+        return len(self._vehicles) / (self._img.shape[0] * self._img.shape[1])
