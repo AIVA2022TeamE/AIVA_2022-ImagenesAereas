465da6192e25c1cee71499f591575115b4f9cc23
elenagiraldo3 <elenagiraldo3@gmail.com>
2022-04-06 14:07:49 +0200

    Mejorar la estructura. Issue #18

--- src/Vehicle.py
+++ src/Vehicle.py
@@ -1,23 +1,24 @@
 from typing import Tuple
 
 
+# def set_coordinates(x: float, y: float):
+"""
+Set coordinates in Vehicle object.
+:param float x: x coordinate (West-East).
+:param float y: y coordinate (North-South).
+"""
+# self._coordx = x
+# self._coordy = y
+
+
 class Vehicle:
     def __init__(self, row: int, col: int, width: int, height: int):
         self._row = row
         self._col = col
         self._width = width
         self._height = height
-        self._coordx = None
-        self._coordy = None
-
-    def set_coordinates(self, x: float, y: float):
-        """
-        Set coordinates in Vehicle object.
-        :param float x: x coordinate (West-East).
-        :param float y: y coordinate (North-South).
-        """
-        self._coordx = x
-        self._coordy = y
+        # self._coordx = None
+        # self._coordy = None
 
     def get_image_info(self) -> Tuple:
         """
