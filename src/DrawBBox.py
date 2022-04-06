4f50bd14690ebfcc4de58f51d43a034eac351508
elenagiraldo3 <elenagiraldo3@gmail.com>
2022-04-06 14:10:09 +0200

    Mejora de la estructura. Issue #17

--- src/DrawBBox.py
+++ src/DrawBBox.py
@@ -1,17 +1,17 @@
 from typing import List
-
+import Vehicle
 import cv2 as cv
 import numpy as np
 
 
 class DrawBBox:
-    def draw(self, image: np.ndarray, vehicles: List) -> np.ndarray:
+    @staticmethod
+    def draw(image: np.ndarray, vehicles: List) -> np.ndarray:
         """
         Draw all vehicles in image.
         :param np.ndarray image: The image witch will be used.
         :param List vehicles: The list of all the vehicles.
         """
-
         for v in vehicles:
             row, col, width, height = v.get_image_info()
 
